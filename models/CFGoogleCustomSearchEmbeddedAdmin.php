3fbf4c8aef924b927f95526ac2de87af2e7b01cb
Steven Mathias <steve@crowdfavorite.com>
2012-07-26 11:40:11 -0600

    Initial commit of plugin

---
+++ models/CFGoogleCustomSearchEmbeddedAdmin.php
@@ -0,0 +1,43 @@
+<?php
+
+load_plugin_textdomain('cfgcse');
+class CFGoogleCustomSearchEmbedAdmin {
+
+	public static function adminMenu() {
+		add_submenu_page('options-general.php', __('CF Google Search', 'cfgcse'),
+			__('CF Google Search', 'cfgcse'), 'manage_options', 'cf-google-custom-search',
+			'CFGoogleCustomSearchEmbedAdmin::adminPage'
+		);
+	}
+	
+	public static function adminPage() {
+		if (!empty($_POST['cf_google_custom_search_save']) &&
+			isset($_POST['cf_gcse_engine_id']) &&
+			check_admin_referer('gcse_save_options')
+		) {
+			update_option('_cf_gcse_engine_id', $_POST['cf_gcse_engine_id']);
+			wp_redirect($_SERVER['REQUEST_URI']);
+			die();
+		}
+		$cse_id = get_option('_cf_gcse_engine_id', '');
+		?>
+<?php screen_icon(); ?><h1><?php echo __('CF Google Custom Search Engine Configuration', 'cfgcse'); ?></h1>
+<form action="" method="post" id="cf_google_custom_search_admin" style="max-width: 500px;">
+<?php wp_nonce_field('gcse_save_options'); ?>
+<table class="form-table">
+	<tbody>
+	<tr>
+		<th scope="row"><label for="cf_gcse_remote_id"><?php _e('Search Engine ID', 'cfgcse'); ?>:</label></th>
+		<td><input type="text" id="cf_gcse_engine_id" name="cf_gcse_engine_id" value="<?php echo esc_attr($cse_id); ?>" class="widefat" /></td>
+	</tr>
+	<tr>
+		<th><input type="submit" name="cf_google_custom_search_save" value="<?php echo esc_attr(__('Save', 'cfgcse')); ?>" class="button-primary" /></th>
+		<td></td>
+	</tr>
+	</tbody>
+</table>
+</form>
+<?php
+	}
+}
+add_action('admin_menu', 'CFGoogleCustomSearchEmbedAdmin::adminMenu');
---
+++ models/CFGoogleCustomSearchEmbeddedEndpoint.php
@@ -0,0 +1,79 @@
+<?php
+
+load_plugin_textdomain('cfgcse');
+if (!class_exists('CFGoogleCustomSearchEmbeddedEndpoint')) {
+class CFGoogleCustomSearchEmbeddedEndpoint {
+	public static $term;
+	private static $options = array();
+
+	public static function onSearch($where, $query) {
+		global $wp_the_query;
+		// compare query to wp_the_query to see if it is main. This is for compatibility prior to 3.3
+		// 3.3+ use is_main_query() instead
+		if ($query === $wp_the_query && is_search() && !is_admin()) {
+			remove_filter('posts_where', 'CFGoogleCustomSearchEmbeddedEndpoint::onSearch');
+			self::$term = get_query_var('s');
+			$where = 'WHERE 1 = 0 ';
+			self::$options = apply_filters('cf_gcse_options', self::$options);
+		}
+		return $where;
+	}
+	
+	public static function onBoxShortcode($atts = array()) {
+		$cse_id = get_option('_cf_gcse_engine_id', null);
+		$search_url = apply_filters('cf_gcse_search_url', home_url());
+		$protocol = is_ssl() ? 'https' : 'http';
+	?>
+		<div id="cse-search-form" style="width: 100%;"><?php _e('Loading', 'cfgcse'); ?></div>
+		<script src="<?php echo $protocol; ?>://www.google.com/jsapi" type="text/javascript"></script>
+		<script type="text/javascript"> 
+			google.load('search', '1', {language : 'en', style : google.loader.themes.ESPRESSO});
+			google.setOnLoadCallback(function() {
+				var customSearchOptions = <?php echo json_encode(self::$options); ?>;
+				var customSearchControl = new google.search.CustomSearchControl(
+					<?php echo json_encode($cse_id); ?>, customSearchOptions);
+				customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+				var options = new google.search.DrawOptions();
+				options.setAutoComplete(true);
+				options.enableSearchboxOnly(<?php echo json_encode($search_url); ?>, "s");
+				customSearchControl.draw('cse-search-form', options);
+			}, true);
+		</script>
+	<?php
+	}
+	
+	public static function onResultsShortcode($atts = array()) {
+		$cse_id = get_option('_cf_gcse_engine_id', null);
+		$protocol = is_ssl() ? 'https' : 'http';
+	?>
+		<div id="cse" style="width: 100%;"><?php _e('Loading', 'cfgcse'); ?></div>
+		<script src="<?php echo $protocol; ?>://www.google.com/jsapi" type="text/javascript"></script>
+		<script type="text/javascript"> 
+			google.load('search', '1', {language : 'en', style : google.loader.themes.ESPRESSO});
+			google.setOnLoadCallback(function() {
+				var customSearchOptions = {};
+				<?php if (!empty(self::$options)) { ?>
+				customSearchOptions[google.search.Search.RESTRICT_EXTENDED_ARGS] = <?php echo json_encode(self::$options); ?>;
+				<?php } ?>
+				var customSearchControl = new google.search.CustomSearchControl(
+					<?php echo json_encode($cse_id); ?>, customSearchOptions);
+				customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+				var options = new google.search.DrawOptions();
+				options.setAutoComplete(true);
+				customSearchControl.setLinkTarget(google.search.Search.LINK_TARGET_SELF);
+				customSearchControl.draw('cse', options);
+				var queryFromUrl = <?php echo json_encode(self::$term); ?>;
+				if (queryFromUrl) {
+					customSearchControl.execute(queryFromUrl);
+				}
+			}, true);
+		</script>
+ 	<?php
+	}
+}
+
+add_filter('posts_where', 'CFGoogleCustomSearchEmbeddedEndpoint::onSearch', 10, 2);
+add_shortcode('cf-google-search-embedded-box', 'CFGoogleCustomSearchEmbeddedEndpoint::onBoxShortcode');
+add_shortcode('cf-google-search-embedded-results', 'CFGoogleCustomSearchEmbeddedEndpoint::onResultsShortcode');
+
+}

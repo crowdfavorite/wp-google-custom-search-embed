22074826466ca465e3af588d2f03191be34b4519
Matt Richmond <matt@crowdfavorite.com>
2012-10-08 17:04:54 -0600

    better directory name

--- cf-google-custom-search-embed.php
+++ cf-google-custom-search-embed.php
@@ -10,8 +10,8 @@ Author URI: http://crowdfavorite.com/
 
 load_plugin_textdomain('cfgcse');
 define('CF_GCSE_URL', apply_filters('cf_gcse_url', plugins_url(basename(dirname(__file__))), basename(dirname(__file__))));
-include dirname(__file__).'/models/CFGoogleCustomSearchEmbeddedEndpoint.php';
+include dirname(__file__).'/lib/CFGoogleCustomSearchEmbeddedEndpoint.php';
 
 if (is_admin()) {
-	include dirname(__file__).'/models/CFGoogleCustomSearchEmbeddedAdmin.php';
+	include dirname(__file__).'/lib/CFGoogleCustomSearchEmbeddedAdmin.php';
 }
